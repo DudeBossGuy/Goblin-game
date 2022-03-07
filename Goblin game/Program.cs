cac4144963f8303259e33ff542886e40315d3bf5
Christopher Dimattina <dim0031@nhs.vic.edu.au>
2022-03-08 10:28:58 +1100

    Changing Movement system

--- Goblin game/Program.cs
+++ Goblin game/Program.cs
@@ -13,21 +13,37 @@ namespace goblingame
             bool HasBow;
             bool HasKey;
             int Arrows;
+            int Potions;
+            string Answer;
+            byte Health;
+            byte Room;
             Console.WriteLine("Hello, welcome to Goblin Village, goblins have taken over the world.");
-            Thread.Sleep(1000);
+            Console.ReadLine();
             Console.WriteLine("you job is to go kill everyone");
-            Thread.Sleep(1000);
+            Console.ReadLine();
             Console.WriteLine("have this sword");
-            Thread.Sleep(1000);
+            Console.ReadLine();
             HasSword = true;
             Item sword = new();
             sword.Name = "sword";
             sword.Description = "the sword is sharp, you should go kill some gobos with it";
             sword.Damage = 5;
             sword.Get();
-            Console.WriteLine("there are two doors one to your right and one to your right which way do you go?");
-            
-            
+            Map one = new();
+            one.InitMap();
+            one.RoomPath[0, 1] = true;
+            one.RoomPath[1, 3] = true;
+
+            one.RoomPath[1, 2] = true;
+            one.RoomPath[5, 0] = true;
+
+            one.RoomPath[5, 3] = true;
+            one.RoomPath[4, 1] = true;
+            one.Spawn = 0;
+            one.End = 4;
+            one.Nav();
+
+
 
         }
         public class Item
@@ -39,10 +55,8 @@ namespace goblingame
             {
                 Console.ForegroundColor = ConsoleColor.Yellow;
                 Console.WriteLine("You now have a " + Name);
-                Thread.Sleep(1000);
                 Console.ForegroundColor = ConsoleColor.Green;
                 Console.WriteLine(Description);
-                Thread.Sleep(1000);
                 Console.ForegroundColor = ConsoleColor.DarkCyan;
                 Console.WriteLine("it does " + Damage + " damage");
                 Console.ResetColor();
@@ -50,38 +64,69 @@ namespace goblingame
         }
         public class Map
         {
-            public string answer;
-            public bool option1;
-            public bool option2;
-            public bool option3;
-            public bool option4;
-            public string option5;
+            public string Answer;
+            public int Direction;
+            public int Room;
+            public int Spawn;
+            public int End;
+            public string 
+            public int[] RoomGobiLev;
+            public int[] RoomLootLev;
+            public bool[,] RoomPath;
+            public void InitMap()
+            {
+                RoomPath = new bool[16, 4];
+                RoomGobiLev = new int[16];
+                RoomLootLev = new int[16];
+            }
             public void Nav()
             {
-                Console.WriteLine("Do you want to");
-                if (option1)
+                int RoomDir;
+                Room = Spawn;
+                Direction = 0;
+                while (Room != End && Answer != "yes")
                 {
-                    Console.WriteLine("go back");
-                }
-                if (option1)
-                {
-                    Console.WriteLine("go back");
-                }
-                if (option1)
-                {
-                    Console.WriteLine("go back");
-                }
-                if (option1)
-                {
-                    Console.WriteLine("go back");
-                }
-                if (option1)
-                {
-                    Console.WriteLine("go back");
-                }
-                Console.WriteLine();
+                    Console.WriteLine("Do you want to:");
+                    for (RoomDir = 0; RoomDir < 4; RoomDir++)
+                    {
+                        if (RoomPath[Room, RoomDir])
+                        {
+                            if (RoomDir == 0)
+                                Console.WriteLine("Go north?");          
+                            if (RoomDir == 1)
+                                Console.WriteLine("Go east?");
+                            if (RoomDir == 2)
+                                Console.WriteLine("Go south?");
+                            if (RoomDir == 3)
+                                Console.WriteLine("Go west?");
+                        }
+                    }
+                    if (Room == End)
+                        Console.WriteLine("there is a portal here do you want to go through it?");
+                    Answer = Console.ReadLine();
+                    Answer = Answer.ToLower();
+                    if (Answer.Contains("north") || Answer == "n")
+                    {
+                        Room = Room - 4;
+                        Console.WriteLine("you head north");
+                    }
+                    if (Answer.Contains("east") || Answer == "e")
+                    {
+                        Room = Room + 1;
+                        Console.WriteLine("you head east");
+                    }
+                    if (Answer.Contains("south") || Answer == "s")
+                    {
+                        Room = Room + 4;
+                        Console.WriteLine("you go south");
+                    }
+                    if (Answer.Contains("west") || Answer == "w")
+                    {
+                        Room = Room + 1;
+                        Console.WriteLine("you head west");
+                    }
 
-
+                }
             }
         }
     }
